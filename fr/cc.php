68ff7c9597039d502945f3486b0928894d552772
Lionel Walter <lionel.walter@epfl.ch>
2012-08-06 13:52:35 +0200

    create mobile page for CC conference

---
+++ fr/cc.php
@@ -0,0 +1,99 @@
+﻿<!DOCTYPE html>
+<html>
+  <head>
+  <title>EPFL Bibliothèque - Conférence Creative Commons</title>
+  <?php
+	include("../include/html/header.html");
+  ?>  
+</head>
+<body data-theme="l">
+
+
+<div data-role="page" id="cc" data-add-back-btn="true">
+<div data-role="header" data-theme="l">
+    <h1>Conférence Creative Commons</h1>
+	<a href="../" data-icon="home" data-iconpos="notext" data-direction="reverse">Home</a>
+</div>
+<div data-role="content">	
+	
+	
+<h2>Partager, télécharger, utiliser légalement... Grâce aux Creative Commons !</h2>
+
+<p>Rencontre organisée par la Bibliothèque de l'EPFL à l'occasion du dixième anniversaire des licences Creative Commons.</p>
+
+<p><strong>Date :</strong> Jeudi 25 octobre de 15h00 à 19h00</p>
+
+<p><strong>Lieu :</strong> <a href="access.php">Forum du Rolex Learning Center</a>, campus de l'EPFL (Lausanne)</p>
+
+<p>Entrée gratuite</p>
+
+<p><a href="http://inform.epfl.ch/index.php?form=Creative_Commons" data-role="button" data-inline="true">Inscription</a> </p>
+
+
+
+<h2>Programme provisoire</h2>
+
+<dl>
+    <dt>15h00</dt>
+    <dd><b>Accueil général</b></dd>
+
+    <dt>15h30</dt>
+    <dd><b>Films introductifs</b><br>
+    </dd>
+
+    <dt>15h45</dt>
+    <dd><b>Présentation de la rencontre</b><br>
+    <b>Isabelle Kratz</b>, Directrice de la Bibliothèque de l'EPFL.</dd>
+
+    <dt>16h00</dt>
+    <dd><b>Conférence-débat 1 : <i>Le projet Creative Commons et ses licences</i></b><br>
+    <b>Lara Beswick</b>, Coordinatrice du groupe art et musique de Creative Commons France.</dd>
+
+    <dt>17h00</dt>
+    <dd><b>Stands et en-cas offert</b><br>
+    (Liste non exhaustive des stands ; des mises à jour seront faites ultérieurement.)<br>
+    <b>Paul Oberson</b>, <a href="http://craft.epfl.ch/page-48182.html">Centre de Recherche et d'Appui pour la Formation et ses Technologies</a> (CRAFT), EPFL, Lausanne.<br>
+    <b>Michael Schär</b> et <b>Davide Longo</b>, <a href="http://www.schaermoments.com/">Schaermoments</a>, Label de musique, Lausanne.<br>
+    <b>Patrick Johner</b>, <a href="http://icp.ge.ch/sem/prestations/spip.php?article97">SEM Documentation</a>, Département de l'instruction publique, de la culture et du sport, Genève.<br>
+    <b>Maude Thomas</b>, <a href="http://www.wikivalais.ch/">Wikivalais</a>, Médiathèque du Valais, Sion.<br>    
+    <b>Raphaël Grolimund</b> et <b>Aurore Granval</b>, Bibliothèque de l'EPFL, Lausanne.<br>
+    </dd>
+
+    <dt>18h00</dt>
+    <dd><b>Conférence-débat 2 : <i>Creative Commons@CERN: le partage des connaissances accélère la science</i></b><br>
+    <b>Diane Nourissier Sirakov</b>, Juriste du CERN, Genève.</dd>
+</dl>
+
+
+
+<h2>Contact</h2>
+
+<ul data-role="listview" role="listbox">
+
+<li>
+<a href="mailto:questions.bib@epfl.ch">questions.bib@epfl.ch</a>
+</li>
+
+<li>
+<a href="tel:+41 21 6932156">+41 21 6932156</a>
+</li>
+
+<li><a href="access.php">Plan d'accès</a></li>
+
+
+</ul> 
+
+
+
+
+	
+	
+	
+</div><!-- /content -->
+
+
+
+</div>
+
+</body>
+</html>
